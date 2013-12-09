43257eab49f974e424014ea5cd58fa9d4426acb3
Thomas Lein <thomaslein@gmail.com>
2013-12-09 15:19:42 +0100

    Using spaces instead og tabs

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var mongo_provider = require('./lib/mongoprovider').MongoProvider,
 server_config = {
-	host:'localhost',
-	port:'27017',
-	db:'db'
+  host:'localhost',
+  port:'27017',
+  db:'db'
 }
 
 mongo_provider = new MongoProvider(server_config , function(error, connected){
-	if(error) throw error
-	console.log('CONNECTED: ', connected);
-});
+  if(error) throw error
+  console.log('CONNECTED: ', connected);
+}); 
 
